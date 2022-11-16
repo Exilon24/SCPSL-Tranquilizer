84ab562b0b7fa554f139f7b0da8b124879f7623b
Exilon <explunki@outlook.com>
2022-11-16 19:51:50 +0400

    Added the main scripts.

---
+++ Plugin.cs
@@ -0,0 +1,21 @@
+﻿namespace SCPSLTranquilizer
+{
+    using System;
+    using Exiled.API.Features;
+
+    public class Plugin : Plugin<Config>
+    {
+        public override string Name => "SCP:SL Tranquilizer";
+        public override string Author => "(S)Exilon";
+        public override Version Version => new Version(0, 0, 0);
+        public override void OnEnabled()
+        {
+            ;
+        }
+
+        public override void OnDisabled()
+        {
+            ;
+        }
+    }
+}
