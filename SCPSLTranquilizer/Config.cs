11cb5065460300d5b9097d85e795a0b504f99c08
Exilon <explunki@outlook.com>
2022-11-17 17:05:21 +0400

    re-added project files.

---
+++ SCPSLTranquilizer/Config.cs
@@ -0,0 +1,20 @@
+﻿namespace SCPSLTranquilizer
+{
+    using Exiled.API.Interfaces;
+    using System.ComponentModel;
+
+    public class Config : IConfig
+    {
+        [Description("Whether the plugin is enabled")]
+        public bool IsEnabled { get; set; }
+
+        [Description("How long should an SCP be knocked out (s)")]
+        public int SCPKnockoutTime { get; set; } = 30;
+
+        [Description("How long should a human be knocked out (s)")]
+        public int HumanKnockoutTime { get; set; } = 60;
+
+        [Description("Should 096 be affected by the tranquilizer (if true, he will not be enraged")]
+        public bool pacify096 { get; set; } = true;
+    }
+}
---
+++ SCPSLTranquilizer/Plugin.cs
@@ -0,0 +1,24 @@
+﻿namespace SCPSLTranquilizer
+{
+    using Exiled.API.Features;
+    public class Plugin : Plugin<Config>
+    {
+        // Shit I have to override for the plugin to work
+        public override string Name => "SCP:SL Tranquilizer";
+        public override string Author => "(S)Exilon";
+        public override Version Version => new Version(1, 0, 0);
+
+        public override void OnDisabled()
+        {
+            Log.Info($"SCP Knockout time in seconds: {Config.SCPKnockoutTime}");
+            Log.Info($"Human Knockout time in seconds: {Config.HumanKnockoutTime}");
+            Log.Info($"Will 096 be pacified: {Config.pacify096}");
+
+        }
+
+        public override void OnEnabled()
+        {
+            ;
+        }
+    }
+}
