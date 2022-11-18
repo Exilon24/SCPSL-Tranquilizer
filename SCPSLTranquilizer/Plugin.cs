6af325de9439633869f0755d40e46ad00d42473e
Exilon <explunki@outlook.com>
2022-11-18 09:10:54 +0400

    Made the mod. I just gotta test now.

--- SCPSLTranquilizer/Plugin.cs
+++ SCPSLTranquilizer/Plugin.cs
@@ -1,15 +1,19 @@
 ﻿namespace SCPSLTranquilizer
 {
-    using Exiled.Events.Handlers;
     using Exiled.API.Features;
     using MEC;
+    using PlayerStatsSystem;
+    using System.Collections.Generic;
+
 
     public class Plugin : Plugin<Config>
     {
         // Shit I have to override for the plugin to work
         public override string Name => "SCP:SL Tranquilizer";
         public override string Author => "(S)Exilon";
-        public override Version Version => new Version(1, 0, 0);
+        public override System.Version Version => new System.Version(1, 0, 0);
+
+        public bool canEnrage = true;
 
         public override void OnDisabled()
         {
@@ -22,8 +26,30 @@
         public override void OnEnabled()
         {
             Exiled.Events.Handlers.Player.Shot += Player_Shot;
+            Exiled.Events.Handlers.Player.PickingUpItem += Player_PickingUpItem;
+            Exiled.Events.Handlers.Scp096.Enraging += Scp096_Enraging;
             new Broadcast("The mod works!", 20, true);
             
+        }
+
+        private void Scp096_Enraging(Exiled.Events.EventArgs.EnragingEventArgs ev)
+        {
+            if (!canEnrage)
+            {
+                ev.IsAllowed = false;
+            }
+            else
+            {
+                ev.IsAllowed = true;
+            }
+        }
+
+        private void Player_PickingUpItem(Exiled.Events.EventArgs.PickingUpItemEventArgs ev)
+        {
+            if (ev.Pickup.Type == ItemType.GunCOM15)
+            {
+                ev.Player.Broadcast(new Broadcast($"You picked up the <color=red>tranquilizer</color>", 5, true));
+            }
         }
 
         private void Player_Shot(Exiled.Events.EventArgs.ShotEventArgs ev)
@@ -36,11 +62,18 @@
 
                 if (ev.Target.IsScp)
                 {
-                    knockout(true, ev);
+                    // Check if the SCP is 096
+                    if (ev.Target.Role.Type == RoleType.Scp096 && Config.pacify096)
+                    {
+                        ev.Target.Broadcast(new Broadcast("You have been pacified!", 5, true));
+                        knockout(true, true, ev);
+                    }
+
+                    knockout(true, false, ev);
                 }
                 else
                 {
-                    knockout(false, ev);
+                    knockout(false, false, ev);
                 }
                
             }
@@ -52,18 +85,32 @@
         /// <param name="isSCP"></param>
         /// <param name="ev"></param>
         /// <returns></returns>
-        public IEnumerator<float> knockout(bool isSCP, Exiled.Events.EventArgs.ShotEventArgs ev)
+        public IEnumerator<float> knockout(bool isSCP, bool is096, Exiled.Events.EventArgs.ShotEventArgs ev)
         {
             ev.Target.CanSendInputs = false;
             ev.Target.IsInvisible = true;
-            
 
+            // Spawn ragdoll
+            Ragdoll playerRagdoll = new Ragdoll(new RagdollInfo(Exiled.API.Features.Server.Host.ReferenceHub, new UniversalDamageHandler(200, DeathTranslations.Crushed), ev.Target.Position, default), true);
+            playerRagdoll.Spawn();
+           
             if (isSCP)
             {
-                Log.Warn("SCP Tranquilized");
-                yield return Timing.WaitForSeconds((float)Config.SCPKnockoutTime);
-                ev.Target.CanSendInputs = true;
-                ev.Target.IsInvisible = false;
+                if (is096)
+                {
+                    Log.Warn("096 Tranquilized");
+                    canEnrage = false;
+                    yield return Timing.WaitForSeconds((float)Config.SCPKnockoutTime);
+                    canEnrage = true;
+                }
+                else
+                {
+                    Log.Warn("SCP Tranquilized");
+                    yield return Timing.WaitForSeconds((float)Config.SCPKnockoutTime);
+                    ev.Target.CanSendInputs = true;
+                    ev.Target.IsInvisible = false;
+                    playerRagdoll.Delete();
+                }
             }
             else 
             {
@@ -71,6 +118,7 @@
                 yield return Timing.WaitForSeconds((float)Config.HumanKnockoutTime);
                 ev.Target.CanSendInputs = true;
                 ev.Target.IsInvisible = false;
+                playerRagdoll.Delete();
             }
         }
     }
